dedb8bbcef0535af110c1b0c618cf682d22539dc
Deepu <deepu2005g@gmail.com>
2026-04-17 21:43:40 +0530

    task1 and task2

---
+++ Level_1/task1.py
@@ -0,0 +1,6 @@
+def reverse_string(s):
+    return s[::-1]
+
+text = input("Enter a string to reverse: ")
+result = reverse_string(text)
+print("Reversed string: ", result)
---
+++ Level_1/task2.py
@@ -0,0 +1,27 @@
+def celsius_to_fahrenheit(c):
+    return (c * 9/5) + 32
+
+def fahrenheit_to_celsius(f):
+    return (f - 32) * 5/9
+
+while True:
+    try:
+        value = float(input("Enter the temperature value: "))
+        break
+    except ValueError:
+        print("Please enter a valid temperature value")
+
+while True:  
+    unit = input("Enter the unit (C/F): ").upper()
+
+    if unit == "C":
+        result = celsius_to_fahrenheit(value)
+        print("Fahrenheit: ", result)
+        break
+    elif unit == "F":   
+        result = fahrenheit_to_celsius(value)
+        print("Celsius: ", result)
+        break
+    else:
+        print("Please enter a valid unit")
+    
